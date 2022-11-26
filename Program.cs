e323ba0f31f803d019daaee96a03f125746f693d
Raisa01~ <zarvirova@mail.ru>
2022-11-26 21:35:50 +0300

    Tasks 19.21.23

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ int ReadInt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }*/
 //Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-int number = ReadInt("Введите число N: ");
+/*int number = ReadInt("Введите число N: ");
 for (int i = 1; i <= number; i++)
 { 
     Console.Write($"{i*i*i} ");
@@ -48,4 +48,4 @@ int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
-}
+}*/
